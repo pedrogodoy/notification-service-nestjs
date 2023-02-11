8ef1d607ad386b1816f0551fcaa9cd412a3a6ac3
Pedro <pedrogh.godoy12@gmail.com>
2023-02-10 22:53:48 -0300

    feat: create db model

--- dist/src/infra/database/database.module.js
+++ dist/src/infra/database/database.module.js
@@ -9,8 +9,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.DatabaseModule = void 0;
 const common_1 = require("@nestjs/common");
 const notifications_repository_1 = require("../../application/repositories/notifications-repository");
+const products_repository_1 = require("../../application/repositories/products-repository");
 const prisma_service_1 = require("./prisma/prisma.service");
 const prisma_notifications_repository_1 = require("./prisma/repositories/prisma-notifications-repository");
+const prisma_products_repository_1 = require("./prisma/repositories/prisma-products-repository");
 let DatabaseModule = class DatabaseModule {
 };
 DatabaseModule = __decorate([
@@ -19,8 +21,13 @@ DatabaseModule = __decorate([
             {
                 provide: notifications_repository_1.NotificationsRepository,
                 useClass: prisma_notifications_repository_1.PrismaNotificationsRepository,
-            }],
-        exports: [notifications_repository_1.NotificationsRepository]
+            },
+            {
+                provide: products_repository_1.ProductsRepository,
+                useClass: prisma_products_repository_1.PrismaProductsRepository,
+            }
+        ],
+        exports: [notifications_repository_1.NotificationsRepository, products_repository_1.ProductsRepository]
     })
 ], DatabaseModule);
 exports.DatabaseModule = DatabaseModule;
---
+++ prisma/migrations/20230211014605_added_products_schema/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "price" TEXT NOT NULL,
+    "sku" TEXT NOT NULL
+);
